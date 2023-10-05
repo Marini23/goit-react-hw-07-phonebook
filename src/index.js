cf13825f2f65405dbc260132f3f840c491a04062
Marini23 <marina.smile1m@gmail.com>
2023-10-05 18:20:47 +0200

    1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store, persistor } from 'components/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-06-phonebook">
+    <BrowserRouter basename="/goit-react-hw-07-phonebook">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
